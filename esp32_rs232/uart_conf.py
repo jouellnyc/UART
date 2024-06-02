8c43317b7f8ab79d22458f428cb1ec9d83f3eb98
JohnO <32470508+jouellnyc@users.noreply.github.com>
2024-06-02 18:43:55 -0400

    Create uart_conf.py

---
+++ esp32_rs232/uart_conf.py
@@ -0,0 +1,8 @@
+import machine
+import time
+
+# Configure ESP32 pins (replace with actual pins you're using)
+tx_pin = 13  # Typically connected to DE and TxD of MAX485
+rx_pin = 14  # Typically connected to RE and RxD of MAX485
+uart = machine.UART(2, baudrate=9600, tx=tx_pin, rx=rx_pin)
+me="esp-XXX"
