ecdc6dc1b0a8f6ba70c9b862798d65332ff6545c
John Ouellette <jouellnyc@gmail.com>
2024-07-04 23:35:54 -0400

    add new slave

---
+++ rainbow/slave_pico2_eth/oled_pins.py
@@ -0,0 +1,3 @@
+from machine import Pin
+scl=Pin(27)
+sda=Pin(26)
---
+++ rainbow/slave_pico2_eth/rgb_pin_defs.py
@@ -0,0 +1,4 @@
+from machine import Pin
+red_pin    = Pin(2, Pin.OUT)
+green_pin  = Pin(28, Pin.OUT)
+blue_pin   = Pin(22, Pin.OUT)
