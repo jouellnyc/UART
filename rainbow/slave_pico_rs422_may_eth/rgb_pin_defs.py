28b5d12846008177749a389d113f7bdea58d034b
John Ouellette <jouellnyc@gmail.com>
2024-07-07 23:09:41 -0400

    +personalities

--- rainbow/slave_pico_rs422_may_eth/rgb_pin_defs.py
+++ rainbow/slave_pico_rs422_may_eth/rgb_pin_defs.py
@@ -1,4 +1,4 @@
 from machine import Pin
-red_pin    = Pin(18, Pin.OUT)
-green_pin  = Pin(17, Pin.OUT)
-blue_pin   = Pin(16, Pin.OUT)
+red_pin    = Pin(13, Pin.OUT)
+green_pin  = Pin(12, Pin.OUT)
+blue_pin   = Pin(11, Pin.OUT)
