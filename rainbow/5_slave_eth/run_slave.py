ce06408b4befa52bd05d07fdfba1ee658ba70fd9
John Ouellette <jouellnyc@gmail.com>
2025-03-31 18:51:56 -0400

    Timeout and Verbosity fixes

--- rainbow/5_slave_eth/run_slave.py
+++ rainbow/5_slave_eth/run_slave.py
@@ -6,15 +6,15 @@ from www_cli import get_rgb_from_http
 
 while True:
 
-    rgb_tuple = get_rgb_from_http(url,verbose=True)
-    print(time.localtime())    
+    print(f"== {time.localtime()}")
+    rgb_tuple = get_rgb_from_http(url, verbose=False, timeout=3)
     if rgb_tuple:
         if rgb_tuple == (9,9,9):
             clear()
         else:
-            print(f"=={rgb_tuple}==")
+            print(f"=={rgb_tuple}==\n")
             set_color(*rgb_tuple)
             show_rgb_text(rgb_tuple)
     else:
-        print(f"No data at {time.localtime()}")
+        print(f"No data retrieved\n")
     time.sleep(1)       
--- rainbow/5_slave_eth/www_cli.py
+++ rainbow/5_slave_eth/www_cli.py
@@ -18,17 +18,22 @@ def http_tuple(rgb_tuple_string):
         rgb_tuple = tuple(int(value) for value in rgb_tuple_values)
         return rgb_tuple
 
-def get_rgb_from_http(url,verbose=False):
+def get_rgb_from_http(url, verbose=True, timeout=3):
     try:
         if verbose:
             print(f"Connecting to {url}") 
-        rgb_tuple = urequests.get(url).text
+        rgb_tuple = urequests.get(url, timeout=timeout).text
+    except OSError as oe:
+        if 'ETIMEDOUT' in str(oe):
+            print(f"Timeout to {url}: appears inaccessible")
+            return None
     except Exception as e:
+        print(f"Unhandled Error: {e}")
         return None
     else:
+        ht = http_tuple(rgb_tuple)
         if verbose:
-            print(f"HTTP RGB TUPLE: {rgb_tuple}",type(rgb_tuple))
-            ht = http_tuple(rgb_tuple)
+            print(f"HTTP RGB TUPLE: {rgb_tuple}", type(rgb_tuple))
         return ht
     
     
